5110effc74a55f71554a1650ebc4cd6a41a49097
Sankalp Gunturi <sgunturi@protonmail.com>
2026-03-06 03:25:39 +0530

    Revert "Add working V1"

--- src/popup/index.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { PopupApp } from "./App";
-import "../styles/popup.css";
-
-createRoot(document.getElementById("root")!).render(<PopupApp />);
